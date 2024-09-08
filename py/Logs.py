49db0cfd7765bc563f43241356752a14b24ebf3b
Ian <imallarino2015@fau.edu>
2024-09-08 01:15:03 -0400

    Update Logs.py

--- py/Logs.py
+++ py/Logs.py
@@ -2,6 +2,7 @@ import os
 import time
 from datetime import datetime
 import traceback
+from inspect import stack
 
 
 class BusinessException(Exception):
@@ -10,24 +11,48 @@ class BusinessException(Exception):
 
 
 class Logger:
+    severity_levels = [
+        "Trace", "Info",
+        "Warning", "Critical", "Error",
+        "Business Exception", "System Exception"
+    ]
     class Log:
         def __init__(
                 self, message: str, severity: str,
-                exception_type=None, stacktrace=None,
+                exception_type: str | None = None,
+                stacktrace: str | None = None,
                 timestamp: datetime | None = None
         ):
             self.timestamp = datetime.now() if timestamp is None else timestamp
             self.message = message
             self.severity = severity
-            self.stacktrace = stacktrace
             self.exception_type = exception_type
+            self.stacktrace = stacktrace
 
-        def __str__(self):
+        def print(self):
+            severity_colors = {
+                "Trace": "", "Info": "",
+                "Warning": "\033[93m", "Critical": "\033[31m", "Error": "\033[30m\033[41m",
+                "Business Exception": "\033[30m\033[43m", "System Exception": "\033[30m\033[41m"
+            }
+            print(f"{severity_colors[self.severity]}{self}\033[0m")
 
-            return f"{self.message}" + ('\n' + self.stacktrace if self.stacktrace is not None else '')
+        def __str__(self):  # TODO: add coloring to str based on severity
+            ret_string = ""
+
+            ret_string += (self.exception_type + ': ' if self.exception_type is not None else '')
+            ret_string += self.message
+            ret_string += ('\n' + self.stacktrace if self.stacktrace is not None else '')
+
+            return ret_string
 
         def __repr__(self):
-            return f"{self.timestamp} - {self.severity}:\n{self}"
+            ret_string = ""
+
+            ret_string += f"{self.timestamp} - {self.severity}:"
+            ret_string += f"\n{self}"
+
+            return ret_string
 
         def __iter__(self):
             yield "timestamp", self.timestamp.isoformat()
@@ -38,48 +63,50 @@ class Logger:
 
     def __init__(self):
         self.logs = []
-        self.severity_levels = [
-            "Trace", "Info", "Warning", "Critical", "Error",
-            "Business Exception", "System Exception"
-        ]
         self.minimum_print_severity = 1
         self.working_dir = os.getcwd()
 
-    def _log(self, *messages: str, sep: str = " ", severity: int = 1, exception_type=None, stacktrace=None):
+    def _log(
+            self, *messages: str,
+            sep: str = "", severity: int = 1,
+            exception_type=None, stacktrace=None
+    ):
         log = self.Log(
             sep.join([str(message) for message in messages]),
             self.severity_levels[severity],
-            stacktrace, exception_type
+            exception_type, stacktrace
         )
         self.logs.append(log)
         if severity >= self.minimum_print_severity:
-            print(log, "\033[0m", sep="")
+            log.print()
 
-    def log_trace(self, *messages: str, sep: str = " "):
-        self._log(sep.join([message for message in messages]), sep="", severity=0)
+    def log_trace(self, *messages: str, sep: str = ""):
+        self._log(*messages, sep=sep, severity=0 )
 
-    def log_info(self, *messages: str, sep: str = " "):
-        self._log(sep.join([message for message in messages]), sep="", severity=1)
+    def log_info(self, *messages: str, sep: str = ""):
+        self._log(*messages, sep=sep, severity=1)
 
-    def log_warning(self, *messages: str, sep: str = " "):
-        self._log("\033[93m", sep.join([message for message in messages]), sep="", severity=2)
+    def log_warning(self, *messages: str, sep: str = ""):
+        self._log(*messages, sep=sep, severity=2)
 
-    def log_critical(self, *messages: str, sep: str = " "):
-        self._log("\033[31m", sep.join([message for message in messages]), sep="", severity=3)
+    def log_critical(self, *messages: str, sep: str = ""):
+        self._log(*messages, sep=sep, severity=3)
 
-    def log_error(self, *messages: str, sep: str = " "):
-        self._log("\033[30m", "\033[41m", sep.join([message for message in messages]), sep="", severity=4)
+    def log_error(self, *messages: str, sep: str = ""):
+        self._log(*messages, sep=sep, severity=4)
 
     def log_business_exception(self, exception: Exception):
         self._log(
-            "\033[30m", "\033[43m", str(exception), sep="", severity=5,
-            exception_type="Business Exception"
+            str(exception), sep="", severity=5,
+            exception_type=type(exception).__name__,
+            stacktrace=''.join(traceback.format_exception(exception))
         )
 
     def log_system_exception(self, exception: Exception):
         self._log(
-            "\033[30m", "\033[41m", str(exception), sep="", severity=6,
-            exception_type=f"System Exception - {exception}", stacktrace=''.join(traceback.format_exception(exception))
+            str(exception), sep="", severity=6,
+            exception_type=type(exception).__name__,
+            stacktrace=''.join(traceback.format_exception(exception))
         )
 
     def log_exception(self, exception: Exception):
@@ -87,11 +114,6 @@ class Logger:
             self.log_business_exception(exception)
         else:
             self.log_system_exception(exception)
-
-    def save_plain_text(self, file_name: str):
-        with open(file_name, "w+") as file:
-            for log in self.logs:
-                file.write(f"{log}\n")
 
     def to_json(self):
         return [dict(log) for log in self.logs]
@@ -103,7 +125,7 @@ class Logger:
         return self.logs[idx]
 
     def __str__(self):
-        return str([repr(log) for log in self.logs])
+        return "\n".join([str(log) for log in self.logs])
 
 
 def call_with_logs(f: callable, logger: Logger) -> any:
@@ -114,7 +136,7 @@ def call_with_logs(f: callable, logger: Logger) -> any:
 
 
 def retry(
-        f: callable, logger: Logger,
+        f: callable, logger: Logger | None = None,
         expected_val: any = None,
         retry_action: callable = lambda: None,
         retry_delay: float = 0, max_retries: int = 3
@@ -130,7 +152,8 @@ def retry(
             if retries >= max_retries:
                 raise e
             retry_action()
-            logger.log_exception(e)
+            if logger is not None:
+                logger.log_exception(e)
             retries += 1
             time.sleep(retry_delay)
             print(f"Retrying: {retries}")
@@ -143,8 +166,8 @@ def main():
     system_logger.log_trace("trace")
     system_logger.log_info("info")
     system_logger.log_warning("warning")
-    system_logger.log_error("error")
     system_logger.log_critical("critical")
+    system_logger.log_error("error")
 
     try:
         raise BusinessException("Business exception")
@@ -153,6 +176,16 @@ def main():
 
     try:
         raise Exception("System exception")
+    except Exception as e:
+        system_logger.log_exception(e)
+
+    try:
+        print(1/0)
+    except Exception as e:
+        system_logger.log_exception(e)
+
+    try:
+        print(3 + "x")
     except Exception as e:
         system_logger.log_exception(e)
     return
