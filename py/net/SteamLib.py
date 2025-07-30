30d0b04bc1086fb1e0787ee26bf5e24f47714fd2
Ian <imallarino2015@fau.edu>
2025-07-30 19:50:20 -0400

    Update SteamLib.py

--- py/net/SteamLib.py
+++ py/net/SteamLib.py
@@ -12,28 +12,35 @@ TODO:
 
 
 class SteamGame(object):
+    store_page = None
+    application_config = None
+
     id = None
-    name = None
     link = None
 
-    has_demo = None
+    name = None
+    description = None
+    publish_date = None
+
     base_price = None
     discount = None
     final_price = None
 
     bundles = None
 
-    platforms = None
-    publish_date = None
     is_early_access = None
+    has_demo = None
+
+    platforms = None
+    sd_compatibility = None
 
     review_total = None
     review_recent = None
 
-    sd_compatibility = None
-
-    store_page = None
-    application_config = None
+    features = None
+    controllers = None
+    accessibility_features = None
+    drm_notices = None
 
     def __init__(self, game_name=None, game_id=None):
         assert game_name is not None or game_id is not None
@@ -47,9 +54,11 @@ class SteamGame(object):
 
             if self.name is None:
                 self.fetch_name()
+            self.fetch_description()
             self.fetch_bundle_info()
             self.fetch_publish_date()
             self.fetch_early_access_status()
+            self.fetch_features()
             self.fetch_review()
             self.fetch_price_info()
             self.fetch_platforms()
@@ -140,21 +149,33 @@ class SteamGame(object):
     def fetch_name(self):
         self.update_store_page()
 
-        self.name = self.store_page.find("div", attrs={"id": "appHubAppName"}).text
+        self.name = self.store_page.find("div", attrs={"id": "appHubAppName"}).text.strip()
+
+        return None
+
+    def fetch_description(self):
+        self.update_store_page()
+
+        self.description = self.store_page.find("div", attrs={"class": "game_description_snippet"}).text.strip()
 
         return None
 
     def fetch_price_info(self):
-        self.base_price, self.discount, self.final_price = ("", "0", "")
+        # self.base_price, self.discount, self.final_price = ("", "0", "")
         self.update_store_page()
 
         game_area_purchase = self.store_page.find("div", attrs={"id": "game_area_purchase"})
 
         self.has_demo = game_area_purchase.find("div", attrs={"class": "demo_above_purchase"}) is not None
 
+        free_game_button = game_area_purchase.find("div", attrs={"id": "freeGameBtn"})
+        if free_game_button is not None:
+            self.final_price = " ".join([item for item in game_area_purchase.find("div", attrs={"class": "game_purchase_price price"}).text.split() if item != ""])
+            return None
+
         coming_soon_area = game_area_purchase.find("div", attrs={"class": "game_area_comingsoon"})
         if coming_soon_area is not None:
-            self.final_price = coming_soon_area.find("h1").text
+            self.final_price = " ".join([item for item in coming_soon_area.find("h1").text.split() if item != ""])
             return None
 
         game_area_wrappers = game_area_purchase.find_all("div", attrs={"class": "game_area_purchase_game_wrapper"})
@@ -180,11 +201,52 @@ class SteamGame(object):
                 self.base_price = price_element.find("div", attrs={"class", "discount_original_price"}).text.strip()
                 self.discount = price_element.find("div", attrs={"class", "discount_pct"}).text.strip()
                 self.final_price = price_element.find("div", attrs={"class", "discount_final_price"}).text.strip()
+            else:
+                self.base_price = game_purchase_elements[1].find("div", attrs={"class", "discount_original_price"}).text.strip()
+                self.discount = game_purchase_elements[1].find("div", attrs={"class", "discount_pct"}).text.strip()
+                self.final_price = game_purchase_elements[1].find("div", attrs={"class", "discount_final_price"}).text.strip()
 
         self.base_price = float(self.base_price.replace("$", "").strip())
         self.discount = float(self.discount.replace("%", "").strip())
         self.final_price = float(self.final_price.replace("$", "").strip())
         # TODO: get currency type
+        return None
+
+    def fetch_early_access_status(self):
+        self.update_store_page()
+
+        self.is_early_access = len(self.store_page.find_all("div", attrs={"class": "early_access_header"})) > 0
+
+        return None
+
+    def fetch_features(self):
+        self.update_store_page()
+
+        feature_element_container = self.store_page.find("div", attrs={"class": "game_area_features_list_ctn"})
+        feature_elements = feature_element_container.find_all("div", attrs={"class": "label"})
+        if len([feature_elements]) > 0:
+            self.features = [
+                feature_element.text.strip()
+                for feature_element
+                in feature_elements
+            ]
+
+        controller_element = self.store_page.find("div", attrs={"data-featuretarget": "store-sidebar-controller-support-info"})
+        if controller_element is not None:
+            self.controllers = json.loads(controller_element.get("data-props"))
+
+        accessibility_feature_element = self.store_page.find("div", attrs={"data-featuretarget": "store-sidebar-accessibility-info"})
+        if accessibility_feature_element is not None:
+            self.accessibility_features = json.loads(accessibility_feature_element.get("data-props"))
+
+        drm_notice_elements = self.store_page.find_all("div", attrs={"class": "DRM_notice"})
+        if len(drm_notice_elements) > 0:
+            self.drm_notices = [
+                drm_notice_element.text.strip().replace(b"\xa0".decode(encoding="windows-1252"), " ")
+                for drm_notice_element
+                in drm_notice_elements
+            ]
+
         return None
 
     def fetch_platforms(self):
@@ -207,7 +269,7 @@ class SteamGame(object):
 
         self.bundles = [
             {
-                "bundle_name": bundle.find("h1").text.replace("BUNDLE", "").replace("(?)", "").strip(),
+                "bundle_name": bundle.find("h2").text.replace("BUNDLE", "").replace("(?)", "").strip(),
                 **json.loads(bundle["data-ds-bundle-data"])
             }
             for bundle
@@ -224,18 +286,11 @@ class SteamGame(object):
 
         return None
 
-    def fetch_early_access_status(self):
-        self.update_store_page()
-
-        self.is_early_access = len(self.store_page.find_all("div", attrs={"class": "early_access_header"})) > 0
-
-        return None
-
     def fetch_review(self):
         self.update_store_page()
 
         review_elem = self.store_page.find("div", attrs={"id": "userReviews"})
-        match review_elem.find_all("div", attrs={"class": "user_reviews_summary_row"}):
+        match review_elem.find_all("a", attrs={"class": "user_reviews_summary_row"}):
             case [x]:
                 if "data-tooltip-html" in x:
                     self.review_total = (
@@ -243,8 +298,15 @@ class SteamGame(object):
                         x["data-tooltip-html"].strip().split()[3]
                     )
                 else:
-                    self.review_total = None
-                self.review_recent = None
+                    self.review_total = (
+                        x.find("span", attrs={"class": "not_enough_reviews"}).text.strip()
+                        if x.find("span", attrs={"class": "not_enough_reviews"}) is not None
+                        else None,
+                        x.find("span", attrs={"class": "responsive_reviewdesc"}).text.strip()
+                        if x.find("span", attrs={"class": "responsive_reviewdesc"}) is not None
+                        else None
+                    )
+                self.review_recent = (None, None)
             case [x, y, *_]:
                 self.review_recent = (
                     x["data-tooltip-html"].strip().split()[0],
@@ -254,24 +316,38 @@ class SteamGame(object):
                     y["data-tooltip-html"].strip().split()[0],
                     y["data-tooltip-html"].strip().split()[3]
                 )
+            case _:
+                pass
 
         return None
 
     def __iter__(self):
-            yield "Game Name",                self.name
             yield "ID",                       self.id
             yield "Link",                     self.link
+
+            yield "Name",                     self.name
+            yield "Description",              self.description
+            yield "Publish Date",             self.publish_date
+
             yield "Base Price",               self.base_price
-            yield "Early Access",             self.is_early_access
-            yield "Steam Deck Compatibility", self.sd_compatibility
-            yield "Has Demo",                 self.has_demo
             yield "Discount %",               self.discount
             yield "Final Price",              self.final_price
+
             yield "Bundles",                  self.bundles
+
+            yield "Early Access",             self.is_early_access
+            yield "Has Demo",                 self.has_demo
+
             yield "Platforms",                self.platforms
-            yield "Publish Date",             self.publish_date
+            yield "Steam Deck Compatibility", self.sd_compatibility
+
             yield "Overall Review",           self.review_total
             yield "Recent Review",            self.review_recent
+
+            yield "Features",                 self.features
+            yield "Controllers",              self.controllers
+            yield "Accessibility Features",   self.accessibility_features
+            yield "DRM Notices",              self.drm_notices
 
     def __str__(self):
         game_details = dict(self)
